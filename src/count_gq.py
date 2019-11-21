46a51c908a7c73505f8c103058784934e432eb7a
Joaquin <jperezdeayala@gmail.com>
2019-11-21 17:51:53 +0100

    1

--- src/count_gq.py
+++ src/count_gq.py
@@ -3,6 +3,6 @@ def count(collection, lista):
     list250 = []
     list500 = []
     for e in lista:
-        list250.append(gq.geoQuery(collection,e,250,0))
-        list500.append((gq.geoQuery(collection,e,500,250))/2)
+        list250.append(len(gq.geoQuery(collection,e,250,0)))
+        list500.append(len((gq.geoQuery(collection,e,500,250)))/2)
     return list250, list500
